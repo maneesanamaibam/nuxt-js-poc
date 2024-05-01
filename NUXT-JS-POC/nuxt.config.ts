cd3563482b9a57f25ee38e2b2f50f65756a60895
Maibam Maneesana <mmaneesanasingh@covalience.com>
2024-05-01 19:51:52 +0530

    configure nuxt config for CSRF issue on deployment production

--- NUXT-JS-POC/nuxt.config.ts
+++ NUXT-JS-POC/nuxt.config.ts
@@ -25,7 +25,7 @@ export default defineNuxtConfig({
     },
     public: {
       authJs: {
-        // baseUrl: process.env.NUXT_NEXTAUTH_URL, // The URL of your deployed app (used for origin Check in production)
+        baseUrl: process.env.NUXT_NEXTAUTH_URL, // The URL of your deployed app (used for origin Check in production)
         verifyClientOnEveryRequest: true, // whether to hit the /auth/session endpoint on every client request
       },
     },
